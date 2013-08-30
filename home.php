a96fb9d0b5a733f6fe5311c33063931241818077
Guri Sogn Andersen <guri.s.andersen@gmail.com>
2013-08-30 20:16:26 +0200

    Archive dropdown instead of list

--- home.php
+++ home.php
@@ -40,9 +40,10 @@
 	<div class="row">
 	<div class="span4">
 		<h2>by Date:</h2>
-		<ul>
-			<?php wp_get_archives('type=monthly'); ?>
-		</ul>
+		<select name="archive-dropdown" onchange="document.location.href=this.options[this.selectedIndex].value;">
+  		<option value=""><?php echo esc_attr( __( 'Select Month' ) ); ?></option> 
+				  <?php wp_get_archives( array( 'type' => 'monthly', 'format' => 'option', 'show_post_count' => 1 ) ); ?>
+		</select>
 	</div> <!--#span-->
 
 	<div class="span4">		
