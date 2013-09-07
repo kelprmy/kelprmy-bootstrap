3f249f8a7a8de6087218975fecf19af4fa3f76b3
Guri Sogn Andersen <guri.s.andersen@gmail.com>
2013-09-07 11:22:34 +0200

    post-page fixed

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<div class="span8">
+<div class="container">
+<div class="row">
+  <div class="col-md-8">
 
 	<!--Post info-->
 	<div class="row">
@@ -20,16 +22,13 @@
 		</div>	
 		
 		<div class="row">
-			</br>
+
 			<!-- Flattr -->
 			<script id='flattrbtn'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=kelpRmy&button=compact&url='+encodeURIComponent(document.URL);f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('flattrbtn');</script> 
 		</div>
 		
-		<div class="row">
 		<!--Back button-->
-		<span><a href="http://guriandersen.no/my-blog/" class="btn btn-inverse pull-right"><i class="icon-rotate-left"></i> Back </a></span>
-		</br>
-		</div>
+		<span><a href="http://guriandersen.no/my-blog/" class="btn btn-primary pull-right"><i class="icon-rotate-left"></i> Back </a></span>
 		
 		<div class="row">
 		<!--Social share-->
@@ -39,19 +38,18 @@
 		<p><?php comments_template(); ?></p>
 		</div>
 		
-</div> <!-- span -->
+  </div> <!-- col-md-8-->
 
 <!-- sidebar -->
-<div class="row">
-  <div class="span4 offset8">
+  <div class="col-md-4">
 	<?php get_sidebar(); ?>	
   </div>
 </div>  
-
-
-<div class="row">
-  <div class="span8">
-	<?php get_footer(); ?>
-  </div>
 </div>
+
+
+<div class="container">
+	<?php get_footer(); ?>
+</div>
+
 
