0fd33774019718ff5308ca537986a598c6d34fcb
Guri Sogn Andersen <guri.s.andersen@gmail.com>
2013-09-08 10:06:59 +0200

    meta tag for scaling on mobile

--- header.php
+++ header.php
@@ -7,6 +7,9 @@
 	
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	
+	<!-- mobile intitial scale -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta charset="utf-8">
 	
